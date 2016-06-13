ae093be560c8cb96856881c42cb94e305147edbf
pbuttler <pdb7705@g.rit.edu>
2016-06-13 15:50:28 -0600

    Fixed creation of chirps
    I forgot how class instantiate works in python.

--- chirpClass.py
+++ chirpClass.py
@@ -1,9 +1,10 @@
-class Chirp(parent):
+class Chirp(object):
     """This is a class whose members are messages It contains info on how to deliver and respond to its contained data"""
-    creator = object
+    creator = null
     label = "A new chirp"
-    def __init__(self,*args):
+    def __init__(self,creator,*args):
         self.args = args
+        self.creator = creator
     def __repr__(self):
         return label
 
