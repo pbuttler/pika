9f149e16132d11ed9b7ca06bd00b64793d84638b
pbuttler <pdb7705@g.rit.edu>
2016-06-13 15:43:08 -0600

    Made the pika class
    A pika is a class whose members are the "things" of the environment. (Users, Docs, lists)

---
+++ pikaClass.py
@@ -0,0 +1,9 @@
+class Pika():
+    """A class for things that "exist""""
+    name = ""
+    def __init__(self,*args):
+        self.args = args
+    def __repr__(self):
+        return name
+    def makeChirp(self,*args):
+        newChirp = new Chirp(self)
