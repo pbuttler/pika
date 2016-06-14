2341cb0156f05ab29844a705ffd4006fd4dde524
pbuttler <pdb7705@g.rit.edu>
2016-06-14 15:06:17 -0600

    Added data variable and reverb function

--- chirpClass.py
+++ chirpClass.py
@@ -1,7 +1,10 @@
 class Chirp(object):
     """This is a class whose members are messages It contains info on how to deliver and respond to its contained data"""
     creator = null
-    label = "A new chirp"
+    label = "A new chirp"               #A name for this chirp
+    data = {}                           #The content of this chirp
+    def reverb():
+        #Rules on how to react to this chirp
     def __init__(self,creator,*args):
         self.args = args
         self.creator = creator
