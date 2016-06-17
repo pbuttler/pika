5886f66f5fa64e41b39ffb2fb55f6fff18c184a9
pbuttler <pdb7705@g.rit.edu>
2016-06-17 13:24:55 -0600

    Created the pikaCommand module

---
+++ pikaCommand.py
@@ -0,0 +1,18 @@
+"""
+This is a module for commanding the pikas via user input
+Here a user can:
+- Select a pika
+- Command a selected pika
+- Create new pikas
+- Destroy pikas :(
+"""
+
+#User input loop
+
+#Variables
+selectedPika = null
+
+#Functions
+def createPika():
+
+def destroyPika
